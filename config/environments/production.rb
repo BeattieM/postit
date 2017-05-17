1834ad9fda6b73762ce33283fd955e2a5a7bea0e
Michael Beattie <beattie@cloudspace.com>
2017-05-17 11:28:09 -0400

    Added basic Devise user auth

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,6 +40,20 @@ Rails.application.configure do
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+  config.action_mailer.default_options = { from: 'noreply@gentle-spire-38369.herokuapp.com' }
+
+  config.action_mailer.default_url_options = { host: 'gentle-spire-38369.herokuapp.com' }
+
+  config.action_mailer.smtp_settings = {
+    port: ENV['MAILGUN_SMTP_PORT'],
+    address: ENV['MAILGUN_SMTP_SERVER'],
+    user_name: ENV['MAILGUN_SMTP_LOGIN'],
+    password: ENV['MAILGUN_SMTP_PASSWORD'],
+    domain: 'gentle-spire-38369.herokuapp.com',
+    authentication: :plain
+  }
+  
+  ActionMailer::Base.delivery_method = :smtp
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
